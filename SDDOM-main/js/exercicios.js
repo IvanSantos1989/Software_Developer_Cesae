e30eed965822a5a1c81f2477453abdf07ca0af72
ivan <ivanlpsantos@hotmail.com>
2025-06-16 19:24:19 +0100

    exs js

--- SDDOM-main/js/exercicios.js
+++ SDDOM-main/js/exercicios.js
@@ -168,3 +168,36 @@ function calcular() {
 
     document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
 }
+
+let myForm = document.querySelector('form');
+
+// myForm.addEventListener('submit', function(e){
+//   e.preventDefault()
+//   console.log('cucu, faz validações e coisas de ui/ux, nomeadamente bloquear o botão');
+
+//   const animalVal = document.querySelector('#animal').value;
+//   const myList = document.querySelector('#zoo-animals');
+//   const myLi = document.createElement('li');
+//   myLi.textContent = animalVal;
+
+
+//   myList.appendChild(myLi)
+
+//   myForm.reset();
+//   //esperamos que o servidor nos dê ok que recebeu os dados
+//   //myForm.submit();
+// })
+
+let myFormG = document.getElementById('myShoppingList');
+
+myFormG.addEventListener('submit', function(e){
+  e.preventDefault();
+  const formData = new FormData(this);
+
+  const li = document.createElement('li');
+  const myUl = document.querySelector('ul');
+  li.innerText = formData.get('product') + '-' + formData.get('qt');
+  myUl.appendChild(li)
+
+  myFormG.reset();
+});
