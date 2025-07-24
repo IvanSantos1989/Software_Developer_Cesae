f733070e99fd8c239a15a07fd7b59f0e309757e2
Ivan <ivanlpsantos@hotmail.com>
2025-07-24 23:14:33 +0100

    Angular and TypeScript

---
+++ Angular_TypeScript/TypeScript/script.js
@@ -0,0 +1,12 @@
+"use strict";
+//alert('Hello TypeScript!')
+//let firstName: string = 'John'
+//let lastName: string = 'Doe'
+//alert(`Hello, ${firstName} ${lastName}!`)
+let firstNumber = 0;
+let secondNumber = 0;
+let total = 0;
+firstNumber = Number(prompt('Please enter the first number:'));
+secondNumber = Number(prompt('Please enter the second number:'));
+total = firstNumber + secondNumber;
+alert(`The total of ${firstNumber} and ${secondNumber} is: ${total}`);
