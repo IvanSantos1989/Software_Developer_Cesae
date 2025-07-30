5fb02e62fe05d1a035b95855d88a39fe32e6b9e4
ivan <ivanlpsantos@hotmail.com>
2025-07-31 00:51:39 +0100

    single page Audi

--- Angular_TypeScript/Audi/src/app/app.component.ts
+++ Angular_TypeScript/Audi/src/app/app.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CardComponent} from './card/card.component';
-import {CardsComponent} from './cards/cards.component';
 import {HeaderComponent} from './master-page/header/header.component';
 import {FooterComponent} from './master-page/footer/footer.component';
+import {CardsComponent} from './components/cards/cards.component';
+import {CarouselComponent} from './components/carousel/carousel.component';
+import {ImagesComponent} from './components/images/images.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent, CardsComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CardsComponent, CarouselComponent, ImagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'basics19';
-  firstName: string = 'Ivan';
-  lastName: string = 'S. Silva';
+  title = 'Audi';
 }
---
+++ Angular_TypeScript/Audi/src/app/components/images/images.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-images',
+  imports: [],
+  templateUrl: './images.component.html',
+  styleUrl: './images.component.scss'
+})
+export class ImagesComponent {
+  @Input() src = '';
+  @Input() alt = '';
+
+}
