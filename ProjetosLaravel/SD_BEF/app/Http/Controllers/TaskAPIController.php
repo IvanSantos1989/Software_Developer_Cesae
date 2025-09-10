0e17b03a6088c5013b9879a66441b1e65d8f7605
ivan <ivanlpsantos@hotmail.com>
2025-09-10 17:32:06 +0100

    laravel updates on the project
    api testing

---
+++ ProjetosLaravel/SD_BEF/app/Http/Controllers/TaskAPIController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Resources\TaskResource;
+
+
+class TaskAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //return Task::all();
+        return Task::paginate();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $task = Task::create($request->all());
+
+        return $task;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task): TaskResource
+    {
+        return new TaskResource($task);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+
+        return response()->json('task actualizada com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json('task eliminada com sucesso!');
+    }
+   
+}
--- ProjetosLaravel/SD_BEF/app/Models/Task.php
+++ ProjetosLaravel/SD_BEF/app/Models/Task.php
@@ -7,11 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 // caso use o método create em vez de insert, descomentar a propriedade fillable
 {
-    // protected $fillable = [
-    //     'name',
-    //     'description',
-    //     'user_id',
-    //     'due_at',
-    //     'status'
-    // ];
+    protected $fillable = [
+        'name',
+        'description',
+        'user_id'
+    ];
 }
